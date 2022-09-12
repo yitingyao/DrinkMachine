5fd2f589516f033872b211ba307bb752ffe22c74
Yiting Yao <yao00043@algonquinlive.com>
2022-09-12 19:07:03 -0400

    Removed some comments

--- src/Notes/Assignment2.java
+++ src/Notes/Assignment2.java
@@ -1,17 +1,5 @@
 package Notes;
 
-/*
- *Student Name: Yiting Yao
- *Student Number: 041-035-917
- *Lab Professor: Professor Mel Sanschagrin
- *Due Date: Nov 19, 2021
- *Description: Assignment 2 (Fall 21)
- */
-import java.util.Scanner;
-
-/*
- * This class has the main method for the application
- */
 public class Assignment2 {
 
 	public static void main(String[] args) {
@@ -29,23 +17,6 @@ public class Assignment2 {
 		} while (user.inputString("Continue Program (yes/no):").equals("yes")); // while equals yes, loop gets executed
 																				// again
 		System.out.println("Program has exited.");
-		// ToDo:
-		// Ask for drink size as text, prompt using the menu generated
-		// from a method in DrinkMachine
-
-		// Ask for measured volume, accepting decimal places
-
-		// determine if the volume matches the expected size using the
-		// verifySize() method, printing the returned report.
-
-		// print out your name on screen
-
-		// ask if there is more data to enter (yes, no) and continue
-		// using a loop only if the user enters some form of yes
-		// e.g. "YES" "yEs" "yeS" would be acceptable (any capitalization)
-
-		// you can use any decision structure or loop for this program,
-		// however a for-loop is not recommended.
 
 	}
 }
